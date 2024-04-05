4943ea8c33efc7b6e0949252b3f1db8be4295619
bernardo205 <bernardo.ribeiro.2004@sapo.pt>
2024-04-05 18:00:52 +0100

    header and footer done

--- CemPorcento/obj/Debug/net8.0/CemPorcento.AssemblyInfo.cs
+++ CemPorcento/obj/Debug/net8.0/CemPorcento.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CemPorcento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a6d226b6c428af259839e08ec913d687854ec25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77fbd6741240d7cf8f0b9b733b0387b41d4566ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("CemPorcento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CemPorcento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
